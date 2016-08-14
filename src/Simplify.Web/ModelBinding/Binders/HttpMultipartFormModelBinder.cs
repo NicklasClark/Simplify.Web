804ddc059f4b4d283bca155baac2d65be0690710
Alexanderius <alexander.krylkov@gmail.com>
2016-08-14 22:07:51 +0600

    Initial HttpMultipartFormModelBinder

---
+++ src/Simplify.Web/ModelBinding/Binders/HttpMultipartFormModelBinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Simplify.Web.ModelBinding.Binders
+{
+	/// <summary>
+	/// Provides form multipart data to object binding
+	/// </summary>
+	/// <seealso cref="IModelBinder" />
+	public class HttpMultipartFormModelBinder : IModelBinder
+	{
+		/// <summary>
+		/// Binds the model.
+		/// </summary>
+		/// <typeparam name="T">Model type</typeparam>
+		/// <param name="args">The <see cref="ModelBinderEventArgs{T}" /> instance containing the event data.</param>
+		public void Bind<T>(ModelBinderEventArgs<T> args)
+		{
+			if (args.Context.Request.ContentType.Contains("multipart/form-data"))
+			{
+			}
+
+			throw new NotImplementedException();
+		}
+	}
+}
