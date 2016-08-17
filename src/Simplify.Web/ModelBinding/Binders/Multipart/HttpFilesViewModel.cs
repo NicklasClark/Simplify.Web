f5d13a2e62b7e0237363659eb4a219988b0e92ac
Alexanderius <alexander.krylkov@gmail.com>
2016-08-17 09:57:38 +0600

    Comments and initialization added

--- src/Simplify.Web/ModelBinding/Binders/Multipart/HttpFilesViewModel.cs
+++ src/Simplify.Web/ModelBinding/Binders/Multipart/HttpFilesViewModel.cs
@@ -2,8 +2,14 @@
 
 namespace Simplify.Web.ModelBinding.Binders.Multipart
 {
+	/// <summary>
+	/// Provides HTTP files view model
+	/// </summary>
 	public class HttpFilesViewModel
 	{
-		public IList<HttpFile> Files { get; set; }
+		/// <summary>
+		/// Gets the list of HTTP files.
+		/// </summary>
+		public IList<HttpFile> Files { get; private set; } = new List<HttpFile>();
 	}
 }
